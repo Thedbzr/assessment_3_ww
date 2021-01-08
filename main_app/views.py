8d0a458d7505a8f1e7b6d8c232e5b0457717ca4c
Anthony Martinez <martinezanthony213@gmail.com>
2021-01-08 11:35:28 -0800

    initial commit

---
+++ main_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView
+from .models import Widget
+# Create your views here.
+def index(request):
+    widgets = Widget.objects.all()
+    return render(request, 'index.html', {'widgets': widgets})
